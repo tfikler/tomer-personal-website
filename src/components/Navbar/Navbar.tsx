a0a43ad2cd79ad2253839007a6bec6ae88a9c2a0
Tomer Fikler <88192266+tfikler@users.noreply.github.com>
2024-10-31 18:03:34 +0200

    adding personal image to the top-navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,10 @@ import './Navbar.css'; // Import CSS specific to Navbar
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
-            <div className="logo">Tomer Fikler</div>
+            <div className="logo">
+                <img className="logo-img" src={'/public/personal-picture.svg'} alt={''}/>
+                <span className="logo-text">Tomer Fikler</span>
+            </div>
             <ul className="nav-links">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/projects">Projects</Link></li>
