fa59b0253707f4953f285161b77ee3ee95e9d9dd
Tomer Fikler <88192266+tfikler@users.noreply.github.com>
2024-11-14 11:41:48 +0200

    add option to download my cv

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Box, IconButton, SvgIcon} from '@mui/material';
-import {Facebook, GitHub, LinkedIn} from '@mui/icons-material';
+import {Box, IconButton, SvgIcon, Tooltip} from '@mui/material';
+import {Facebook, GitHub, LinkedIn, ContactPage} from '@mui/icons-material';
 
 const SocialLinks: React.FC = () => {
     return (
@@ -15,6 +15,7 @@ const SocialLinks: React.FC = () => {
                 },
             }}
         >
+            <Tooltip title={"GitHub"} placement={"right"}>
             <IconButton
                 className="social-icon-button"
                 href="https://github.com/tfikler"
@@ -24,6 +25,8 @@ const SocialLinks: React.FC = () => {
             >
                 <GitHub />
             </IconButton>
+            </Tooltip>
+            <Tooltip title={"LinkedIn"} placement={"right"}>
             <IconButton
                 className="social-icon-button"
                 href="https://www.linkedin.com/in/tomer-fikler/"
@@ -33,6 +36,8 @@ const SocialLinks: React.FC = () => {
             >
                 <LinkedIn />
             </IconButton>
+            </Tooltip>
+            <Tooltip title={"Stack Overflow"} placement={"right"}>
             <IconButton
                 className="social-icon-button"
                 href="https://stackoverflow.com/users/22898189/tomer-fikler"
@@ -44,6 +49,8 @@ const SocialLinks: React.FC = () => {
                     <path d="M15 21h-10v-2h10v2zm6-11.665l-1.621-9.335-1.993.346 1.62 9.335 1.994-.346zm-5.964 6.937l-9.746-.975-.186 2.016 9.755.879.177-1.92zm.538-2.587l-9.276-2.608-.526 1.954 9.306 2.5.496-1.846zm1.204-2.413l-8.297-4.864-1.029 1.743 8.298 4.865 1.028-1.744zm1.866-1.467l-5.339-7.829-1.672 1.14 5.339 7.829 1.672-1.14zm-2.644 4.195v8h-12v-8h-2v10h16v-10h-2z" />
                 </SvgIcon>
             </IconButton>
+            </Tooltip>
+            <Tooltip title="Facebook" placement="right">
             <IconButton
                 className="social-icon-button"
                 href="https://facebook.com/tomer.fikler"
@@ -53,6 +60,19 @@ const SocialLinks: React.FC = () => {
             >
                 <Facebook/>
             </IconButton>
+            </Tooltip>
+            <Tooltip title="Download CV" placement="right">
+            <IconButton
+                className="social-icon-button"
+                href='./public/Tomer Fikler-CV.pdf'
+                target="_blank"
+                rel="noopener noreferrer"
+                download='Tomer_Fikler_CV.pdf'
+                sx={{ mb: 2 }}
+            >
+                <ContactPage/>
+            </IconButton>
+            </Tooltip>
         </Box>
     );
 };
